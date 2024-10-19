2e43ea41685c3e274dd96035f2dcb3df6e6779ee
christophergyman <christophergayiuman@gmail.com>
2024-10-19 22:58:51 +0100

    feat: added udp client

---
+++ src/UDP/udp_client.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    char *message = "Hello from client";
+    struct sockaddr_in servaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    socklen_t len = sizeof(servaddr);
+
+    // Send message to server (without MSG_CONFIRM flag)
+    sendto(sockfd, (const char *)message, strlen(message), 0, (const struct sockaddr *)&servaddr, len);
+    printf("Message sent to server.\n");
+
+    // Receive response from server
+    int n = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
+    buffer[n] = '\0';
+    printf("Server: %s\n", buffer);
+
+    close(sockfd);
+    return 0;
+}
